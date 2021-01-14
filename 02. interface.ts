5bfb8a2cdb8d7c1ada5df00cbf672e2c7b5d8247
jk <ecar1234@gmail.com>
2021-01-14 14:26:07 +0900

    Ts 02. interface(proceeding)2

--- 02. interface.ts
+++ 02. interface.ts
@@ -26,9 +26,22 @@ let intro = (info: Exam): {name: string, realAge: number} => {
 }
 let myIntro = intro({name: "JK"});
 
+// Excess property check
+let myIntro2 = intro({nickName: "john"} as Exam)
+//Excess property check2
+interface Exam2 {
+    name?: string;
+    age?: number;
+    [propName: string] : any;
+}
+//Excess property check3
+let examOption = ({nickName: "john", age:30});
+let myIntro3 = (intro(examOption));
+
 //readOnly
 interface Read {
     readonly x: number;
     readonly y: number;
 }
 
+// functional type
