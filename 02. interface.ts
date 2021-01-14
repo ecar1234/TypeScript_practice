bbdab2fb39f557d1b46ccc81fd468a00f3ea5d77
jk <ecar1234@gmail.com>
2021-01-14 14:01:43 +0900

    Ts 02. interface(proceeding)

--- 02. interface.ts
+++ 02. interface.ts
@@ -7,12 +7,24 @@ interface LabeledValue {
 const printLabel = (labelObj:LabeledValue) => {
     console.log(labelObj.label);
 }
+let myObj = {size: 10, label:"hello"};
 
 // Optional properties 
 interface Exam {
     name?: string;
     age? : number;
 }
+let intro = (info: Exam): {name: string, realAge: number} => {
+    let newIntro = {name: "mike", realAge: 24};
+    if(info.name){
+        newIntro.name = info.name;
+    }
+    if(info.age){
+        newIntro.realAge = info.age + 1
+    }
+    return newIntro;
+}
+let myIntro = intro({name: "JK"});
 
 //readOnly
 interface Read {
