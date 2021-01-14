3e6d1f7ac2f8df14449750556f01b3d5706fafcc
jk <ecar1234@gmail.com>
2021-01-14 18:44:33 +0900

    TypeScript practice 02. interface(completed)

--- 02. interface.ts
+++ 02. interface.ts
@@ -44,11 +44,63 @@ interface Read {
     readonly y: number;
 }
 
-//function type
+//function type1
+interface Search {
+    (source: string, subString: string): boolean;
+}
+let mySearch: Search = (source: string, subString: string): boolean => {
+    let result = source.search(subString)
+    return result > -1;
+}
+//function type2
+let mySearch2: Search = (src: string, sub:string) => {
+    let result = src.search(sub);
+    return result > -1;
+}
+//function type3
+let mySearch3: Search = (src, sub) => {
+    let result = src.search(sub);
+    return result > -1;
+}
 
-//indexable types
+//indexable types1
+interface Animal {
+    [index: number]: string;
+}
+let myAniArr: Animal = ["dog", "cat"];
+let myAni: string = myAniArr[0];
+//indexable types2(index 하위타입 사용)
+interface Animal2 {
+    [index: string]: string; // string 가 하위 타입
+    dog: string;
+    cow: number; // error : index의 타입의 하위 타입과 다름
+}
+//indexable types3(index 하위타입 사용)
+interface Animal3 {
+    [index: string]: string | number; // 하위타입을 추가해주면 
+    dog: string;
+    cow: number; // error 가 나지 않음.
+}
+//indexable types4(readonly 사용)
+interface Animal4 {
+   readonly [index: number]: string  // readonly 를 사용해 할당을 막을 수 있다.
+}
+let arrAni: Animal4 = ["bob", "juju"];
+arrAni[2] = "can"; // error : index 할당을 readonly로 막아놔서 error.
 
-//class type
+//class type // extends vs implements 
+// implements an interface
+interface Clock {
+    currentTime: Date;
+    setTime(d: Date): void;
+}
+class NewClock implements Clock{
+    currentTime: Date = new Date();
+    setTime(d: Date) {
+        this.currentTime = d;
+    }
+    constructor(h: number, m: number) { }
+}
 
 //interface extends
 //interface extends(class)
